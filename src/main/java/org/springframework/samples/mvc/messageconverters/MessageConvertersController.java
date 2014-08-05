fbb72e7a62fb21414dbf5f6a6f3fb4d477ac68ec
ittaiz <ittaiz@wix.com>
2014-08-05 12:47:52 +0300

    message converters code removed

--- src/main/java/org/springframework/samples/mvc/messageconverters/MessageConvertersController.java
+++ src/main/java/org/springframework/samples/mvc/messageconverters/MessageConvertersController.java
@@ -1,12 +1,7 @@
 package org.springframework.samples.mvc.messageconverters;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/messageconverters")
