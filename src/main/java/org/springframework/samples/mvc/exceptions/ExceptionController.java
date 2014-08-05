13663e3cda13b32d0299d7d1f4ea543e9556ccc3
ittaiz <ittaiz@wix.com>
2014-08-05 12:46:28 +0300

    exception code removed

--- src/main/java/org/springframework/samples/mvc/exceptions/ExceptionController.java
+++ src/main/java/org/springframework/samples/mvc/exceptions/ExceptionController.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.mvc.exceptions;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,11 +15,6 @@ public class ExceptionController {
 	@RequestMapping("/global-exception")
 	public @ResponseBody String businessException() throws BusinessException {
 		throw new BusinessException();
-	}
-
-	@ExceptionHandler
-	public @ResponseBody String handle(IllegalStateException e) {
-		return "IllegalStateException handled!";
 	}
 
 }
